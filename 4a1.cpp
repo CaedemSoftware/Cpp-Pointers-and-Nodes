50b524e36a895c1318290cdd97459c8e477976fa
Ze Lei <57436044+CaedemSoftware@users.noreply.github.com>
2020-10-20 16:01:32 -0700

    Add files via upload

---
+++ 4a1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string *ptr1 = nullptr;
+	string *ptr2 = nullptr;
+	ptr1 = new string{ "hello" };
+	ptr2 = ptr1;
+	cout << "ptr1: " << *ptr1 << endl;
+	cout << "ptr2: " << *ptr2 << endl;
+	delete ptr1;
+	//delete ptr2;
+	cout << "ptr1: " << ptr1 << endl;
+	cout << "ptr2: " << ptr2 << endl;
+	return  0;
+}
